1cba0afc103dea489b3dda09e9ce03de6c0abf33
Nikos Mpakaris <nikos.mpakaris@gmail.com>
2020-05-25 13:05:43 +0200

    merged Conflicts

--- src/main.java
+++ src/main.java
@@ -9,11 +9,11 @@ public class main {
         Greet.greet(1);
 
         Cheer cheer = new Cheer();
-<<<<<<< HEAD
+
         cheer.cheer("Helló, We are Team awesome!");
 
-=======
-        cheer.cheer("Hallo, We are Team awesome!");
->>>>>>> 610cc414a2f7e8f3d40703e998cc5825e3ed1cba
+
+        
+
     }
 }
