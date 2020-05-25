e6727f508a7efe86138b2c5428b9b3af5a27cdb5
szabosebastian <szabo.sebastian@hotmail.com>
2020-05-25 14:14:19 +0200

    kicked

--- src/main.java
+++ src/main.java
@@ -1,19 +1,18 @@
 public class main {
     public static void main(String[] args) {
         Future future = new Future();
-        future.future("Szebi","Dragon");
+        future.future("Dragon");
 
         Count count = new Count();
-
         count.count(10);
 
-
-
         Greet.greet("Hello");
-        
 
 
         Cheer cheer = new Cheer();
         cheer.cheer("Helló, ", "We are Team awesome!");
+
+        KickMe kickMe = new KickMe();
+        kickMe.kick();
     }
 }
