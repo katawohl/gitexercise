4827e3d9671ac14fccc56383fbc1a31bb62c2ff3
Ákos Demján <akos.demjan@gmail.com>
2020-05-25 10:27:16 +0200

    b1

--- src/main.java
+++ src/main.java
@@ -1,6 +1,9 @@
 public class main {
     public static void main(String[] args) {
+        count();
+    }
 
-
+    public static void count(){
+        System.out.println("My age is 27.");
     }
 }
