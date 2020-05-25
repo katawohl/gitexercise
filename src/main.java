f81c3b8374a975a833aadfa760d0f9296deef694
Nikos Mpakaris <nikos.mpakaris@gmail.com>
2020-05-25 10:03:57 +0200

    clean project

--- src/main.java
+++ src/main.java
@@ -1,12 +1,6 @@
 public class main {
     public static void main(String[] args) {
 
-        System.out.println("Does this work?");
 
-        greetMemberC();
-    }
-
-    public static void greetMemberC () {
-        System.out.println("This is Niko shouting");
     }
 }
