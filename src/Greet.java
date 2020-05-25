52da0bfed634b071a809f5b1dadd911cacfef1f3
Ákos Demján <akos.demjan@gmail.com>
2020-05-25 11:11:21 +0200

    b1

--- src/Greet.java
+++ src/Greet.java
@@ -1,7 +1,7 @@
-public class MemberA {
+public class Greet {
 
-    public static void greet(String whatever){
-        System.out.println("I'm Kata from Group Awesome!");
-    }
+  public static void greet(String whatever) {
+    System.out.println("I'm Kata from Group Awesome!");
+  }
 
 }
